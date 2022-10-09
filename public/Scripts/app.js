09dad3f15644d974bd545969f1d0aff677f1b85e
YSayanthu <113312471+YSayanthu@users.noreply.github.com>
2022-10-09 17:43:10 -0400

    1st commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,8 @@
+// iife --Immediately Invoked Function Expression
+(function () {
+    function Start() {
+        console.log("App  Started..");
+
+    }
+    window.addEventListener("load", start);
+})();
